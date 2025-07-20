be391690707fe7cfd8c5f335ce2d161a77d3b678
rajesh_kumar2136 <rajesh_kumar@unifyed.com>
2025-07-20 14:41:02 +0530

    some changes

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+// Export every component you want to be public but not the stories
+// Export every component you want to be public
+export * from "./Avatar/Avatar";
+export * from "./Badge/Badge";
+export * from "./Button/Button";
+export * from "./ButtonWithDropdown/ButtonWithDropdown";
+export * from "./Breadcrumbs/Breadcrumbs";
+export * from "./Checkbox/Checkbox";
+export * from "./DatePicker/DatePicker";
+export * from "./FileUpload/FileUploadModal"; // Or whichever you prefer
+export * from "./GenericFilter/GenericFilter";
+export * from "./Input/Input";
+export * from "./Modal/Modal";
+export * from "./Pagination/Pagination";
+export * from "./RadioGroup/RadioGroup";
+export * from "./Select/Select";
+export * from "./Sidebar/Sidebar";
+export * from "./Spinner/Spinner";
+export * from "./Textarea/Textarea";
+export * from "./ToggleSwitch/ToggleSwitch";
+export * from "./Tooltip/Tooltip";
+export * from "./WizardModal/WizardModal";
+export * from "./SearchBar/SearchBar";
+export * from "./Spinner/Spinner";
+export * from "./Tooltip/Tooltip";
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
-import path from "node:path";
+import path, { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 const dirname =
@@ -14,7 +15,29 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: "bundle-stats.html", // output file name
+      open: true, // automatically opens the report in your browser after building
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.js"),
+      name: "unifyedx-storybook-new",
+      // ✅ Only build the ES module format
+      formats: ["es"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      // ✅ The 'output.globals' section is no longer needed and can be removed.
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   test: {
     projects: [
       {
